6d8441b8362aafab3c73028bcf9ab004b5e5b9b5
ashley sanchez <130068110+ashaps03@users.noreply.github.com>
2023-12-10 17:18:33 -0500

    -Added function to select the best image quality -Create liked page html and css along with the needed javascript -Created a database to store users liked events -Users are now able to like, unlike and view liked events.

--- TM/models.py
+++ TM/models.py
@@ -1,10 +1,12 @@
-from django.db import models
-
-# Create your models here.
+# Remove the import of EventView from TM.views
+# from TM.views import EventView
+from django.contrib.auth.decorators import login_required
 from django.db import models
 from django.contrib.auth.models import User
-
-from TM.views import EventView
+from django.shortcuts import render
+from django.utils import timezone
+from django.utils.decorators import method_decorator
+from django.views import View
 
 
 class Event(models.Model):
@@ -23,22 +25,26 @@ class Event(models.Model):
         return self.name
 
 
-class FormCollection(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    favorite_artist = models.CharField(max_length=255)
-    favorite_genre = models.CharField(max_length=255)
-    preferred_timeframe_start = models.DateField()
-    preferred_timeframe_end = models.DateField()
 
-    def __str__(self):
-        return f"{self.user.username}'s Form Collection"
-
-
-
-class Favorite(models.Model):
+class LikedEvent(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    event = models.ForeignKey(EventView, on_delete=models.CASCADE)
+    event_name = models.CharField(max_length=255, default='Unknown Event')
+    event_image = models.URLField(default='default_image_url')
+    event_date = models.CharField(max_length=20, default=timezone.now)
+    event_time = models.CharField(max_length=20, default=timezone.now)
+    event_venue = models.CharField(max_length=255, default='Unknown')
+    event_city = models.CharField(max_length=255, default='Unknown')
+    event_state = models.CharField(max_length=255, default='Unknown')
+    event_address = models.CharField(max_length=255, default='')  # Set the default value
+    event_url = models.URLField(default='')
 
     def __str__(self):
-        return f'{self.user.username} likes {self.event.name}'
+        return self.event_name
+@method_decorator(login_required, name='dispatch')
+class LikedEventListView(View):
+    template_name = 'liked_events.html'
 
+    def get(self, request, *args, **kwargs):
+        liked_events = LikedEvent.objects.filter(user=request.user)
+        context = {'liked_events': liked_events}
+        return render(request, self.template_name, context)
